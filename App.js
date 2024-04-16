b02d7a4e134a8b0d9fdbc009641bc7b8b6c85530
manolo <manolomunoz0596@gmail.com>
2024-04-15 21:35:15 -0500

    Avance 1

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, Image, TextInput, Pressable } from "react-native";
+import { STYLES } from "./src/styles/StylesPrincipal";
+
+const img = require("./src/assets/login.png");
+const lock = require("./src/assets/icon-lock.png");
 
 export default function App() {
+  const [text, onChangeText] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={STYLES.container}>
       <StatusBar style="auto" />
+      <Image source={img} style={{ width: 235, height: 213 }} />
+      <Text style={STYLES.title}>Iniciar Sesión</Text>
+      <View style={STYLES.content}>
+        <Text style={STYLES.subTitle}>Usuario</Text>
+        <View style={STYLES.boxInput}>
+          <TextInput
+            style={STYLES.input}
+            onChangeText={onChangeText}
+            value={text}
+            placeholder="Ingresa tu usuario"
+          />
+        </View>
+        <Text>Contraseña</Text>
+        <View>
+          <TextInput style={STYLES.input} secureTextEntry={true} placeholder="Ingresa tu contraseña"/>
+          <Image source={lock} />
+        </View>
+        <View>
+          <Pressable title="Ingresar" value="Ingresar">
+            <Text>Ingresar</Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/StylesPrincipal.js
@@ -0,0 +1,30 @@
+export const STYLES = {
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    marginTop: 40,
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  content: {
+    marginTop: 30
+  },
+  boxInput: {
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: "#00BFA6",
+    borderRadius: 6
+  },
+  input: {
+    width: 285,
+    height: 50
+  },
+  subTitle: {
+    fontSize: 18,
+    fontWeight: '500'
+  },
+};
